83af0c159ebe5257bd2bb0fceee4db2c3cf39ace
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-03-01 11:16:48 +0530

    commit

--- src/main/java/employeems/springboot/controller/EmployeeController.java
+++ src/main/java/employeems/springboot/controller/EmployeeController.java
@@ -1,9 +1,27 @@
 package employeems.springboot.controller;
 
+import employeems.springboot.model.Employee;
+import employeems.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//this is controller class which is basically connect with the database
 
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
+
+//    we call here our repository, since it has all the logics of our programme
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+//    create rest api for get all the employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
 }
