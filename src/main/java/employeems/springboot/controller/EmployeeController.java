0ac4014f9db02c6ac017bc02fce3c896c7bb604d
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-03-16 22:16:47 +0530

    commit

--- src/main/java/employeems/springboot/controller/EmployeeController.java
+++ src/main/java/employeems/springboot/controller/EmployeeController.java
@@ -60,4 +60,14 @@ public class EmployeeController {
 
 //    delete employee rest api
 
+    @DeleteMapping ("/employees/{id}")
+    public ResponseEntity <Map<String,Boolean>> deleteEmployee(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exist with this id!"));
+
+        employeeRepository.delete(employee);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted",Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
 }
