7655da6bc8bd4ef5f51c37ff74f1abfe0f5254fe
Sampath Chathuranga <117255414+sampy99@users.noreply.github.com>
2024-03-18 23:12:51 +0530

    commit

--- src/main/java/employeems/springboot/model/Employee.java
+++ src/main/java/employeems/springboot/model/Employee.java
@@ -27,5 +27,43 @@ public class Employee {
         this.emp_dob = emp_dob;
     }
 
+    public long getEmp_id() {
+        return emp_id;
+    }
 
+    public void setEmp_id(long emp_id) {
+        this.emp_id = emp_id;
+    }
+
+    public String getEmp_name() {
+        return emp_name;
+    }
+
+    public void setEmp_name(String emp_name) {
+        this.emp_name = emp_name;
+    }
+
+    public String getEmp_email() {
+        return emp_email;
+    }
+
+    public void setEmp_email(String emp_email) {
+        this.emp_email = emp_email;
+    }
+
+    public String getEmp_city() {
+        return emp_city;
+    }
+
+    public void setEmp_city(String emp_city) {
+        this.emp_city = emp_city;
+    }
+
+    public float getEmp_dob() {
+        return emp_dob;
+    }
+
+    public void setEmp_dob(float emp_dob) {
+        this.emp_dob = emp_dob;
+    }
 }
